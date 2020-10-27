93e7d21d29ccc6197979a117b88d20d0d3f6c599
Mokyton <72911121+Mokyton@users.noreply.github.com>
2020-10-27 15:08:07 +0300

    Update py.py

--- py.py
+++ py.py
@@ -44,18 +44,6 @@ average_age = (age1+age2+age3+age4)/len(user_list)
 
 print(average_age)
 
-account1 = {'login': 'ivan', 'password': 'q1'}
-account2 = {'login': 'petr', 'password': 'q2'}
-account3 = {'login': 'olga', 'password': 'q3'}
-account4 = {'login': 'anna', 'password': 'q4'}
-
-
-user1 = {'name': 'Иван', 'age': 20, 'account': ['account1']}
-user2 = {'name': 'Петр', 'age': 25, 'account': ['account2']}
-user3 = {'name': 'Ольга', 'age': 18, 'account': ['account3']}
-user4 = {'name': 'Анна', 'age': 27, 'account': ['account4']}
-
-user_list = [user1, user2, user3, user4]
 el1 = user_list[0]
 el2 = user_list[1]
 el3 = user_list[2]
